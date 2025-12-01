2d339b32de6a6aa8dad319e121972e739e3347f1
EliaZelaya <00146322@uca.edu.sv>
2025-12-01 13:49:43 -0600

    add: base de conocimientos de aldemir

---
+++ basedeconocimientos/bas.pl
@@ -0,0 +1,80 @@
+%Aldeas
+aldea(rivoria).
+aldea(montelar).
+
+%Esposos
+esposo(esteban, mariela).
+esposo(andres, clara).
+esposo(rodolfo, teresa).
+esposo(alonso, beatriz).
+esposo(andres, catalina).
+
+%Hijos
+padre(esteban, clara).
+padre(esteban, rodolfo).
+padre(esteban, ismael).
+padre(andres, sofia).
+padre(rodolfo, lucas).
+padre(rodolfo, martina).
+padre(alonso, felipe).
+padre(alonso, catalina).
+padre(alonso, emilia).
+madre(mariela, clara).
+madre(mariela, rodolfo).
+madre(mariela, ismael).
+madre(clara, sofia).
+madre(teresa, lucas).
+madre(teresa, martina).
+madre(beatriz, felipe).
+madre(beatriz, catalina).
+madre(beatriz, emilia).
+
+%Amigos
+amigo(ismael, hector).
+
+%Oficio
+oficio(hector, herrero).
+oficio(teresa, tejedora).
+oficio(clara, maestra).
+oficio(catalina, curandera).
+oficio(felipe, explorador).
+oficio(emilia, ceramista).
+oficio(mateo, agricultor).
+oficio(irene, criadora_cabras).
+oficio(lucas, aprendiz_herrero).
+oficio(andres, comerciante).
+
+% Maestro-Aprendiz
+maestro_de(hector, lucas).
+
+% Alianzas entre aldeas
+alianza(rivoria, durania).
+alianza(durania, rivoria).
+
+% Independencia política
+independiente(montelar).
+
+% Rivalidades personales
+rival(rodolfo, felipe).
+rival(felipe, rodolfo).
+rival(clara, catalina).
+rival(catalina, clara).
+
+% Festividades por aldea
+festividad(rivoria, fiesta_del_rio).
+festividad(montelar, ceremonia_de_la_luna).
+festividad(durania, feria_del_grano).
+
+% Espíritus protectores por aldea
+espiritu_protector(rivoria, espiritu_del_agua).
+espiritu_protector(montelar, espiritu_de_la_montana).
+espiritu_protector(durania, espiritu_de_la_tierra).
+
+% Aspiraciones de los jóvenes
+aspiracion(sofia, aprender_medicina, catalina).
+aspiracion(lucas, continuar_herreria, hector).
+aspiracion(martina, aprender_ceramica, emilia).
+
+% Roles de liderazgo
+lider_aldea(esteban, rivoria).
+lider_aldea(alonso, montelar).
