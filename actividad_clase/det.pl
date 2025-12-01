5663bf46ecc7a8a497f4b2051a95d6d7b113429c
EliaZelaya <00146322@uca.edu.sv>
2025-12-01 13:31:51 -0600

    add: querys

---
+++ actividad_clase/det.pl
@@ -0,0 +1,20 @@
+:-[bas].
+
+
+% Consulta 1
+:-persona(X).
+
+% Consulta 2
+:-motivos(X).
+
+% Consulta 3
+:-estaba(X).
+
+% Consulta 4
+:-oportunidad(X).
+
+% Consulta 5
+:-weapon(X).
+
+% Consulta 6
+:-culpa(X).
